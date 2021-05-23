34655daf85dfcefb86979c0ff2b6edbacc6e6a98
levinericzimmermann <levin.eric.zimmermann@posteo.eu>
2021-05-23 20:18:49 +0200

    Add midi fronted for drone

--- ot2/converters/frontends/midi.py
+++ ot2/converters/frontends/midi.py
@@ -20,3 +20,18 @@ class SustainingInstrumentEventToMidiFileConverter(midi.MidiFileConverter):
 
     def convert(self, event_to_convert: abc.Event) -> abc.Event:
         return super().convert(self._tempo_converter.convert(event_to_convert))
+
+
+class DroneEventToMidiFileConverter(midi.MidiFileConverter):
+    _tempo_converter = symmetrical.tempos.TempoConverter(
+        expenvelope.Envelope.from_levels_and_durations(levels=[30, 30], durations=[1])
+    )
+
+    def __init__(self):
+        super().__init__(
+            path="builds/drone.mid",
+            midi_file_type=0,  # monophon instruments
+        )
+
+    def convert(self, event_to_convert: abc.Event) -> abc.Event:
+        return super().convert(self._tempo_converter.convert(event_to_convert))
