b8e36795aa91f2135155acacc6c1bc719bdad7bf
levinericzimmermann <levin.eric.zimmermann@posteo.eu>
2021-06-06 00:08:43 +0200

    Add new modules to global init file

--- ot2/__init__.py
+++ ot2/__init__.py
@@ -4,3 +4,5 @@ from . import events
 from . import constants
 from . import generators  # depends on constants, events & parameters
 from . import converters
+from . import parts
+from . import render
